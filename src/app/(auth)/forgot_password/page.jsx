3085407bcf848a17b070386fbb956eeaf18728c3
Akhil Verma <akhil.verma@daffodilsw.com>
2024-05-24 17:44:43 +0530

    background color added

--- src/app/(auth)/forgot_password/page.jsx
+++ src/app/(auth)/forgot_password/page.jsx
@@ -75,11 +75,11 @@ const ForgotPassword = () => {
   }, [credential]);
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow dark:border-white md:mt-0 sm:max-w-md xl:p-0 dark:bg-white-800 dark:border-gray-700">
           <div className="p-2 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
               Reset Password
             </h1>
             <TextField
@@ -131,7 +131,7 @@ const ForgotPassword = () => {
         </div>
       </div>
       {loading && <Loader />}
-    </section>
+    </>
   )
 }
 
--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -76,11 +76,11 @@ const Login = () => {
   }, [user]);
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow dark:border-grey md:mt-0 sm:max-w-md xl:p-0 dark:bg-white-800 dark:border-gray-700">
           <div className="p-2 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
               Sign in to your account
             </h1>
             <TextField
@@ -141,7 +141,7 @@ const Login = () => {
         </div>
       </div>
       {loading && <Loader />}
-    </section>
+    </>
   );
 }
 
