f330911c6770a2dc8cf32cf43e80884a0fd2cbed
Akhil Verma <akhil.verma@daffodilsw.com>
2024-05-28 12:30:25 +0530

    commit

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -8,6 +8,7 @@ import { clearSuccess, getUserDetails } from "@/redux/features/auth/authSlice";
 import Loader from "@/components/Loader";
 import { Box, Typography } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "@/redux/hook";
+import Logout from "@/pages/Logout";
 
 const DetailComponent = ({ title, value }) => {
   return (
@@ -67,25 +68,28 @@ const Profile = () => {
     loading ? (
       <Loader />
     ) : (
-      <div className="flex items-center justify-center h-screen">
-        <div className="w-full bg-white rounded-lg shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] dark:border-grey mr-3 ml-3 md:mt-0 sm:max-w-md xl:p-0 dark:bg-white-800 dark:border-gray-700">
-          <div className="p-2 space-y-2 md:space-y-4 sm:p-4">
-            <Typography
-              sx={{
-                fontFamily: "var(--font)",
-                fontSize: "16px",
-                fontWeight: 600,
-                color: "rgb(18, 25, 38)",
-              }}
-            >
-              Personal Details
-            </Typography>
-            <DetailComponent title="Name" value={data?.name} />
-            <DetailComponent title="Mobile Number" value={data?.mobile} />
-            <DetailComponent title="Email" value={data?.email} />
+      <>
+        <div className="flex items-center justify-center h-screen">
+          <div className="w-full bg-white rounded-lg shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] dark:border-grey mr-3 ml-3 md:mt-0 sm:max-w-md xl:p-0 dark:bg-white-800 dark:border-gray-700">
+            <div className="p-2 space-y-2 md:space-y-4 sm:p-4">
+              <Typography
+                sx={{
+                  fontFamily: "var(--font)",
+                  fontSize: "16px",
+                  fontWeight: 600,
+                  color: "rgb(18, 25, 38)",
+                }}
+              >
+                Personal Details
+              </Typography>
+              <DetailComponent title="Name" value={data?.name} />
+              <DetailComponent title="Mobile Number" value={data?.mobile} />
+              <DetailComponent title="Email" value={data?.email} />
+            </div>
+            <Logout />
           </div>
         </div>
-      </div>
+      </>
     )
   );
 };
