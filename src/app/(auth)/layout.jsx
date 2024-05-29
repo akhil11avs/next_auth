4ebd1163acb54a1697e667d27a28d05efbfe4e3c
Akhil Verma <avsakhil11@gmail.com>
2024-05-30 00:27:22 +0530

    commit

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,27 +1,23 @@
-'use client'
-import React from 'react';
+"use client";
+import React from "react";
 
-import Loader from '@/components/Loader';
-import { useAppSelector } from '@/redux/hook';
+import Loader from "@/components/Loader";
+import { useAppSelector } from "@/redux/hook";
 
-import './style.scss';
+import "./style.scss";
 
 const RootLayout = ({ children }) => {
-  const { loading } = useAppSelector(state => state.user);
-  console.log('loading: ', loading);
+  const { loading } = useAppSelector((state) => state.user);
 
-  return (
-    <>
-      <div className="flex h-screen items-center justify-center">
-        <div className="w-full bg-white rounded-lg shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] dark:border-grey mr-3 ml-3 md:mt-0 sm:max-w-md xl:p-0 dark:bg-white-800 dark:border-gray-700">
-          <div className="p-3 space-y-4 sm:p-6">
-            {children}
-          </div>
-        </div>
+  return loading ? (
+    <Loader />
+  ) : (
+    <div className="flex h-screen items-center justify-center">
+      <div className="w-full bg-white rounded-lg shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] dark:border-grey mr-3 ml-3 md:mt-0 sm:max-w-md xl:p-0 dark:bg-white-800 dark:border-gray-700">
+        <div className="p-3 space-y-4 sm:p-6">{children}</div>
       </div>
-      {loading && <Loader />}
-    </>
+    </div>
   )
 }
 
-export default RootLayout
+export default RootLayout;
