40832847eca8d07eaf05e04d64c3f2b88aeea08b
Akhil Verma <akhil.verma@daffodilsw.com>
2024-05-30 00:01:39 +0530

    commit

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,25 +1,48 @@
+'use client'
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import TextField from '@mui/material/TextField';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 import Box from '../Box';
+import Divider from '../Divider';
 
-const InputField = ({ name, type, label, error, size, required, helperText, ...restProps }) => (
-  <Box component="div" noValidate autoComplete="off" sx={{ mb: 2 }} >
-    <TextField
-      type={type}
-      error={error}
-      label={label}
-      id="outlined-start-adornment"
-      helperText={helperText || null}
-      required={required}
-      size={size}
-      name={name}
-      fullWidth
-      {...restProps}
-    />
-  </Box>
-);
+const InputField = ({ name, type, label, error, size, required, helperText, InputProps, ...restProps }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleOnShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <Box component="div" noValidate autoComplete="off" sx={{ mb: 2 }} >
+      <TextField
+        type={showPassword ? 'text' : type}
+        error={error}
+        label={label}
+        id="outlined-start-adornment"
+        helperText={helperText || null}
+        required={required}
+        size={size}
+        name={name}
+        fullWidth
+        InputProps={(type === 'password' ? {
+          endAdornment: (
+            <>
+              <Divider orientation="vertical" variant="middle" flexItem />
+              <div onClick={handleOnShowPassword} role="presentation" style={{ cursor: 'pointer', marginLeft: '10px' }} data-testid="password-visibility-toggle">
+                {!showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+              </div>
+            </>
+          ),
+        } : InputProps)}
+        {...restProps}
+      />
+    </Box>
+  )
+};
 
 InputField.defaultProps = {
   type: 'text',
