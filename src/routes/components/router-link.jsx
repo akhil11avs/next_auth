27f0bd44ac100d05085338ead5376c60bf939f2d
Akhil Verma <avsakhil11@gmail.com>
2024-05-29 21:23:06 +0530

    commit

--- src/routes/components/router-link.jsx
+++
@@ -1,8 +0,0 @@
-import Link from 'next/link';
-import { forwardRef } from 'react';
-
-// ----------------------------------------------------------------------
-
-const RouterLink = forwardRef(({ href, ...other }, ref) => <Link ref={ref} href={href} {...other} />);
-
-export default RouterLink;
